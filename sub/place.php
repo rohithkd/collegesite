1f1264a5f66b39efa1169bc4e71ac5c40e03300a
rohithkd <kdrohith@gmail.com>
2015-05-13 21:29:05 +0530

    first commit

---
+++ sub/place.php
@@ -0,0 +1,38 @@
+<div id="place">
+
+	<div class="box1">
+
+	<div class="line"><h2>Address for Correspondence</h2></div><br>
+  The Principal,<br>
+  Govt. Model Engineering College,<br>
+  Thrikkakara, Kochi,<br>
+  Kerala<br>
+  PIN 682021<br>
+	<div id="contact">
+		<i class="fa fa-phone fa-fw"></i> 0484-2575592<br>
+		
+		<i class="fa fa-envelope fa-fw"></i> principal@mec.ac.in<br>
+		
+	</div>
+	<br>
+
+	<div class="line"><h2>Placement Committee</h2></div><br>
+	<table id="commit">
+		<tr>
+			<td valign="top">Chairman</td>
+			<td class="second">Prof. Dr. V P Devassia<br>Principal, Govt. Model Engg. College<br></td>
+		</tr>
+		<tr>
+			<td valign="top">Placement Officer</td>
+			<td>Dr. Jayasree V K<br>Asst. Professor, Govt. Model Engg. College<br><i class="fa fa-envelope fa-fw"></i>&nbsp;pc@mec.ac.in<br><i class="fa fa-phone fa-fw"></i>&nbsp; +91-9846332111<br></td>
+		</tr>
+		<tr>
+			<td valign="top">Student Coordinators</td>
+			<td>Rajath Rajeev<br><i class="fa fa-envelope fa-fw"></i>&nbsp;rajathrajeev94@gmail.com<br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; rajathrajeev@mec.ac.in<br><i class="fa fa-phone fa-fw"></i>&nbsp;+91-9567397876<br><br>Amaya Shaji<br><i class="fa fa-envelope fa-fw"></i>&nbsp;amayashaji.mec@gmail.com <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;amayashaji@mec.ac.in<br><i class="fa fa-phone fa-fw"></i>&nbsp;+91-9746559222
+      </td>
+		</tr>
+		
+    </table>
+</div>
+ 
+</div>
---
+++ sub/whymec/quote.php
@@ -0,0 +1,122 @@
+			<div class="col-md-12">
+			
+				<div id="cbp-qtrotator" class="cbp-qtrotator">
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/yahoo.jpg" alt="img01" /></div>
+						<blockquote>
+						  <p>Awesome !!!!</p>
+						  <footer>Yahoo</footer>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/factset.png" alt="img02" /></div>
+						<blockquote>
+						  <p>MEC ROCKS!!!</p>
+						  <footer>Factset</footer>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/nvidia.png" alt="img03" /></div>
+						<blockquote>
+						  <p>Really great.<br>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspYou guys go the extra mile. Appreciate it.</p>
+						  <footer>NVIDIA</footer>
+						</blockquote>
+					</div>
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/ericsson.png" alt="img04" /></div>
+						<blockquote>
+						  <p>Unique students with the best skills for networked society.</p>
+						  <footer>Ericsson</footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/outlook.jpg" alt="img04" /></div>
+						<blockquote>
+						  <p>Outlook India ranked Govt. Model Engineering College as the 39th in the list of  Top 75 Engineering Colleges in India of 2014.</p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/outlook.jpg" alt="img04" /></div>
+						<blockquote>
+						  <p>MEC was featured as 5th in terms of Return on Investment by the survey by Outlook.</p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="" alt="" /></div>
+						<blockquote>
+						  <p>The CSR - GHRDC ranking of 2013 placed MEC as 14th among the Top Promising Institutions in the country.</p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/mint.jpg" alt="" /></div>
+						<blockquote>
+						  <p>The final edition of MINT, the business daily of Hindustan Times in collaboration with The Wall Street Journal hailed MEC as 28th among the Top 50 Govt. Engineering Colleges in India.</p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/ghrdc.jpg" alt="img04" /></div>
+						<blockquote>
+						  <p>MEC was ranked 7th among the Top Government Engineering Colleges in India according to the GHRDC.</p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/msp.jpg" alt="" /></div>
+						<blockquote>
+						  <p>The Microsoft Student Partners programme was launched at MEC, the first college to have them in Kerala. </p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+					<div class="cbp-qtcontent">
+						<div class=logos><img src="images/tedx.png" alt="" /></div>
+						<blockquote>
+						  <p>MEC is the first engineering college in India to have hosted TEDx, an independently organized TED event.</p>
+						  <footer></footer>
+						</blockquote>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		
+				
+		<script src="js/jquery.cbpQTRotator.min.js"></script>
+		<script>
+			$( function() {
+				/*
+				- how to call the plugin:
+				$( selector ).cbpQTRotator( [options] );
+				- options:
+				{
+					// default transition speed (ms)
+					speed : 700,
+					// default transition easing
+					easing : 'ease',
+					// rotator interval (ms)
+					interval : 8000
+				}
+				- destroy:
+				$( selector ).cbpQTRotator( 'destroy' );
+				*/
+
+				$( '#cbp-qtrotator' ).cbpQTRotator();
+
+			} );
+		</script>
+			
+		
+
+		</div>
+	
+		</div>
